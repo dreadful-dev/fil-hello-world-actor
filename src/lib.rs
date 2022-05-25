a3eb18d123691e6baab58d02a598601b270a6104
Jacob Krogman <jake@wlabel.co>
2022-05-25 18:52:55 -0400

    create StateObject derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,12 @@ use cid::Cid;
 use fvm_ipld_encoding::tuple::{Deserialize_tuple, Serialize_tuple};
 use fvm_ipld_encoding::{to_vec, CborStore, RawBytes, DAG_CBOR};
 use fvm_sdk as sdk;
-use fvm_sdk::message::NO_DATA_BLOCK_ID;
+use fvm_sdk::NO_DATA_BLOCK_ID;
 use fvm_shared::ActorID;
+
+use fvm_state_macro::StateObject;
+use fvm_state_macro_derive::StateObject;
+
 
 /// A macro to abort concisely.
 /// This should be part of the SDK as it's very handy.
@@ -21,45 +25,9 @@ macro_rules! abort {
 }
 
 /// The state object.
-#[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
+#[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default, StateObject)]
 pub struct State {
     pub count: u64,
-}
-
-/// We should probably have a derive macro to mark an object as a state object,
-/// and have load and save methods automatically generated for them as part of a
-/// StateObject trait (i.e. impl StateObject for State).
-impl State {
-    pub fn load() -> Self {
-        // First, load the current state root.
-        let root = match sdk::sself::root() {
-            Ok(root) => root,
-            Err(err) => abort!(USR_ILLEGAL_STATE, "failed to get root: {:?}", err),
-        };
-
-        // Load the actor state from the state tree.
-        match Blockstore.get_cbor::<Self>(&root) {
-            Ok(Some(state)) => state,
-            Ok(None) => abort!(USR_ILLEGAL_STATE, "state does not exist"),
-            Err(err) => abort!(USR_ILLEGAL_STATE, "failed to get state: {}", err),
-        }
-    }
-
-    pub fn save(&self) -> Cid {
-        let serialized = match to_vec(self) {
-            Ok(s) => s,
-            Err(err) => abort!(USR_SERIALIZATION, "failed to serialize state: {:?}", err),
-        };
-        let cid = match sdk::ipld::put(Code::Blake2b256.into(), 32, DAG_CBOR, serialized.as_slice())
-        {
-            Ok(cid) => cid,
-            Err(err) => abort!(USR_SERIALIZATION, "failed to store initial state: {:}", err),
-        };
-        if let Err(err) = sdk::sself::set_root(&cid) {
-            abort!(USR_ILLEGAL_STATE, "failed to set root ciid: {:}", err);
-        }
-        cid
-    }
 }
 
 /// The actor's WASM entrypoint. It takes the ID of the parameters block,
